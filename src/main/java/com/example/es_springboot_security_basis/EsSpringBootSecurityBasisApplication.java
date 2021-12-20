8ed21c21d2110a00fc0b4b0ad92720cbb0967c5c
Oleg Filatov <fillswim@gmail.com>
2021-12-20 14:10:33 +0300

    Initial commit

---
+++ src/main/java/com/example/es_springboot_security_basis/EsSpringBootSecurityBasisApplication.java
@@ -0,0 +1,13 @@
+package com.example.es_springboot_security_basis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsSpringBootSecurityBasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsSpringBootSecurityBasisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/es_springboot_security_basis/EsSpringBootSecurityBasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.es_springboot_security_basis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsSpringBootSecurityBasisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
